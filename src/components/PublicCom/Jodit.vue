02470493f3fad4e8f4b3d0cedc409f6f0df97e70
zzh <260094892@qq.com>
2021-04-11 15:20:34 +0800

    [modify][jodit上传图片]

--- src/components/PublicCom/Jodit.vue
+++ src/components/PublicCom/Jodit.vue
@@ -26,6 +26,14 @@ export default class JoditC extends Vue {
       minHeight: 500,
       readonly: this.readOnly,
       maxHeight: this.domHeight || 500,
+      //其中图片上传默认是以base64编码的形式存储的,这样的话,在一边文档中有许多图片,就会使得文档变大,不利于存储及显示,我们要改成路径存储在文档中（更多的配置看源码config_1.Config.prototype.uploader对象）
+      // uploader: {
+      //   url: 'http://localhost:8181/index-test.php?action=fileUpload', //一般是oss文件上传服务器的地址，设置了该属性才能支持文件拖拽上传
+      //   headers: null, //请求头设置
+      //   isSuccess: function () {}, //请求成功的方法
+      //   getMessage: function () {}, //获取图片转换后的路径方法
+      //   defaultHandlerSuccess: function () {}, //编辑器中插入图片url图片的方法
+      // },
     })
   }
   //设置内容
