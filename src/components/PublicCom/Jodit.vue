8cc11365f079e49a2b49c8fa893be99f1be09767
zzh <260094892@qq.com>
2021-04-11 11:23:59 +0800

    [modify][jodit配置修改]

--- src/components/PublicCom/Jodit.vue
+++ src/components/PublicCom/Jodit.vue
@@ -1,25 +1,40 @@
 <template>
-  <Row>
+  <Row class="mt10">
     <textarea :id="joditId"></textarea>
   </Row>
 </template>
 <script lang="ts">
 import 'jodit/build/jodit.min.css'
 const { Jodit } = require('jodit/build/jodit.js')
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Prop } from 'vue-property-decorator'
 
 @Component({
   name: 'JoditC',
   components: {},
 })
 export default class JoditC extends Vue {
-  private joditId: string = `jodit${String(new Date().getTime())}` //富文本编辑器id
+  @Prop() domHeight?: number //高度
+  @Prop() readOnly?: boolean //只读
+  private joditId: string = `jodit${String(new Date().getTime())}` //编辑器id
   private JoditEdt: any //jodit配置
-
   private mounted(): void {
     this.JoditEdt = new Jodit(`#${this.joditId}`, {
-      height: '500px',
+      width: '100%',
+      height: this.domHeight || 500,
+      toolbarAdaptive: false, //工具栏是否自适应，默认true
+      language: 'zh_cn',
+      minHeight: 500,
+      readonly: this.readOnly,
+      maxHeight: this.domHeight || 500,
     })
+  }
+  //设置内容
+  private setContent(text: string): void {
+    this.JoditEdt.value = text
+  }
+  //获得内容
+  private getContent(): void {
+    return this.JoditEdt.value
   }
 }
 </script>
--- src/views/Plugins/RichTextEditor/JoditCom.vue
+++ src/views/Plugins/RichTextEditor/JoditCom.vue
@@ -1,5 +1,12 @@
 <template>
-  <Jodit></Jodit>
+  <div>
+    <Jodit ref="joditRef" value=""></Jodit>
+    <Row class="mt10">
+      <Button class="mr10" type="primary" @click="setContent('by-小屁鸿')">设置内容</Button>
+      <Button class="mr10" type="primary" @click="getContent">获取内容</Button>
+      {{'现在内容为：'+content}}
+    </Row>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
@@ -10,5 +17,13 @@ import { Component, Vue } from 'vue-property-decorator'
     Jodit: () => import('@c/PublicCom/Jodit.vue'),
   },
 })
-export default class JoditCom extends Vue {}
+export default class JoditCom extends Vue {
+  private content: string = ''
+  private setContent(val: string): void {
+    ;(this.$refs.joditRef as any).setContent(val)
+  }
+  private getContent(): void {
+    this.content = (this.$refs.joditRef as any).getContent()
+  }
+}
 </script>
